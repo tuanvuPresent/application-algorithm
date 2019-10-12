f6ee4db2e4e10ab056d6abe4bba00d11129290dc
Nguyen Tuan Vu <tuanvubk96@gmail.com>
2019-10-12 23:09:06 +0700

    update

---
+++ exercise/CHANGE.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int coin[]={1,5,10,50,100,500};
+int f[1001];
+
+void creat(int n){
+	f[0] = 0;
+	for(int i=1;i<=n;i++){
+		f[i] = 999999999;
+		for(int j=0;j<6;j++){
+			if(i-coin[j]>=0){
+				f[i] = min(f[i-coin[j]], f[i]) + 1;	
+			}
+		}
+	}
+}
+
+int main(){
+	int n;cin>>n;
+	creat(1000);
+	cout<<f[1000-n];
+	
+	return 0;
+}
+
