21fab30ebbb3a0fbafb1cb0cd5d139852a6d3f78
Nguyen Tuan Vu <tuanvubk96@gmail.com>
2019-11-11 19:46:44 +0700

    update

---
+++ exercise/CONNECTED COMPONENTS.cpp
@@ -0,0 +1,62 @@
+/*
+inputCopy
+8 8
+1 2
+1 7
+1 8
+2 7
+4 5
+4 6
+5 6
+7 8
+outputCopy
+3
+*/
+
+#include<iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+vector <int> v[100001];
+int visit[100001];
+int res = 0;
+int counter = 0;
+
+void isConnect(int n,int x){
+	visit[x] = 1;
+	counter++;
+	if(counter == n){
+		return;
+	}
+	for(int i=0;i<v[x].size();i++){
+		if(visit[v[x][i]] == 0){
+			isConnect(n,v[x][i]);
+		}
+	}
+}
+
+int main(){
+	//input
+	int n,m;cin>>n>>m;
+	for(int i=1;i<=m;i++){
+		int a,b; cin>>a>>b;
+		v[a].push_back(b);
+		v[b].push_back(a);
+	}
+	//handle
+	int i=1;
+	while(i<=n){
+		while(i<=n && visit[i]) i++;
+		if(i<=n){
+			res++;
+			isConnect(n,i);
+		}
+	}
+	//how output
+	cout<<res;
+		
+	return 0;
+}
+
--- mid test 2/Bai4_evenseq.cpp
+++ mid test 2/Bai4_evenseq.cpp
@@ -2,6 +2,9 @@
 5
 -2 1 -4 4 9
 
+5
+9 3 4 7 9
+
 ouput:
 10
 */
@@ -10,26 +13,76 @@ ouput:
 using namespace std;
 
 int arr[100001];
+int NF = -1000000;
+int f[100001];
+int g[100001];
+
+void dynamic(int n){
+	/*
+	f[i] : max chan
+	g[i] : max le
+	*/
+	if(arr[1] % 2 == 0){
+		f[1] = arr[1];
+		g[1] = -1000000;
+	}
+	else{
+		g[1] = arr[1];	
+		f[1] = -1000000;			
+	}
+	for(int i=2; i<=n;i++){
+		if(arr[i] % 2 == 0){
+			if(f[i-1] > NF){
+				f[i] = (f[i-1] + arr[i] > arr[i])? f[i-1] + arr[i] : arr[i];	
+			}else{
+				f[i] = arr[i];
+			}
+			
+			g[i] = (g[i-1] > NF)? g[i] = g[i-1] + arr[i] : NF;
+		}else{
+			if(g[i-1] > NF){
+				f[i] = g[i-1] + arr[i];
+			}else{
+				f[i] = NF;
+			}
+			
+			if(f[i-1]>NF){
+				g[i] = (f[i-1] + arr[i] > arr[i])? f[i-1] + arr[i] : arr[i];
+			}else{
+				g[i] = arr[i];
+			}
+		}
+		cout<<f[i]<<" "<<g[i]<<" "<<"\n";
+	}
+	int res = f[1];
+	for(int i=2;i<=n;i++){
+		if(res < f[i]){
+			res = f[i];
+		}
+	}
+	cout<<res;
+}
 
 int main(){
     int n; cin>>n;
     for(int i=1;i<=n;i++){
     	cin>>arr[i];
-    	arr[i] += arr[i-1];
+//    	arr[i] += arr[i-1];
 	} 
-	
-	int res = -1000000;
-	for(int i=0;i<=n;i++){
-		for(int j=1;j<=n;j++){
-			int sum = arr[j] - arr[i];
-			if(sum % 2 ==0){
-				if(res < sum){
-					res = sum;
-				}
-			}
-		}
-	}
-    cout<<res;
+//	
+//	int res = -1000000;
+//	for(int i=0;i<=n;i++){
+//		for(int j=1;j<=n;j++){
+//			int sum = arr[j] - arr[i];
+//			if(sum % 2 ==0){
+//				if(res < sum){
+//					res = sum;
+//				}
+//			}
+//		}
+//	}
+//    cout<<res;
+    dynamic(n);
 
     return 0;
 }
