3ea60c68266461341589a989638ea98513ec461b
Nguyen Tuan Vu <tuanvubk96@gmail.com>
2019-11-18 13:42:02 +0700

    update

---
+++ exercise/Euler.cpp
@@ -0,0 +1,65 @@
+/*
+input:
+7 12
+1 2
+1 3
+2 3
+2 7
+2 4
+3 7
+3 5
+4 7
+4 5
+4 6
+5 6
+5 7
+*/
+
+#include<iostream>
+#include <algorithm>
+#include <stack>
+#include <set>
+#define MAX 100001
+using namespace std;
+
+set <int> A[MAX];
+stack <int> st,CE;
+int N, M;
+
+void solve(){
+	st.push(1);
+	while(!st.empty()){
+		int x = st.top();
+		if(!A[x].empty()){
+			int y = *(A[x].begin());
+			st.push(y);
+			A[x].erase(y);
+			A[y].erase(x);
+		}else{
+			st.pop();
+			CE.push(x);
+		}
+	}
+}
+
+int main(){
+	//input
+	cin>>N>>M;
+	for(int i=0;i<M;i++){
+		int a,b;
+		cin>>a>>b;
+		A[a].insert(b);
+		A[b].insert(a);
+	}
+	//solve
+	solve();
+	//show output
+	while(!CE.empty()){
+		int x = CE.top();
+		CE.pop();
+		cout<<x<<" ";
+	}
+	
+	return 0;
+}
+
---
+++ exercise/Hamiton.cpp
@@ -0,0 +1,74 @@
+/*
+input:
+7 12
+1 2
+1 3
+2 3
+2 7
+2 4
+3 7
+3 5
+4 7
+4 5
+4 6
+5 6
+5 7
+*/
+#include<iostream>
+#include <algorithm>
+#include <stack> 
+#include <vector>
+#define MAX 100001
+using namespace std;
+
+vector <int> A[MAX];
+int N, M;
+int visited[100001];
+int res[100001];
+
+int check(int x){
+	for(int i=0;i<A[x].size();i++){
+		if(A[x][i] == 1) return 1;
+	}
+	return 0;
+}
+
+void backtrack(int x, int k, int n){
+	if(k == n){
+		if(check(x)){
+			for(int i=0;i<n;i++){
+				cout<<res[i]<<" ";
+			}
+			cout<<"\n";
+		}
+		return;
+	}
+	for(int i=0;i<A[x].size(); i++){
+		int v = A[x][i];
+		if(!visited[v]){
+			visited[v] = 1;
+			res[k] = v;
+			backtrack(v,k+1,n);
+			visited[v] = 0;
+		}
+	}
+}
+
+int main(){
+	//input
+	cin>>N>>M;
+	for(int i=0;i<M;i++){
+		int a,b;
+		cin>>a>>b;
+		A[a].push_back(b);
+		A[b].push_back(a);
+	}
+	//solve
+	visited[1] = 1;
+	res[0] = 1;
+	backtrack(1,1,N);
+	//show output
+	
+	return 0;
+}
+
