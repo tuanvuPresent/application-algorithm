599186d2c0c3c7a74f9972b7fb6472c6cbd47718
Nguyen Tuan Vu <tuanvubk96@gmail.com>
2019-11-25 16:04:01 +0700

    update

---
+++ exercise/Graph/LONGEST PATH ON A TREE.cpp
@@ -0,0 +1,78 @@
+/*
+inputCopy
+6
+1 3 3
+1 6 2
+2 6 5
+4 5 2
+4 6 1
+outputCopy
+10
+
+*/
+#include<iostream>
+#include <algorithm>
+#define MAX 100001
+#include <string.h>
+#include <vector>
+
+using namespace std;
+
+vector <int> A[100001];
+vector <int> d[100001];
+int visited[100001];
+int res = 0;
+//int X[100001];
+int u = 1;
+
+
+int check(int x){
+	for(int i=0;i<A[x].size();i++){
+		if(!visited[A[x][i]]) return 0;
+	}
+	return 1;
+}
+
+void DFS(int x, int k, int n,int sum){
+	if(check(x)){
+		if(res < sum){
+			res = sum;
+			u = x;
+		}
+//		for(int i=0;i<n-1;i++) cout<<X[i]<<" ";
+//		cout<<"\n";
+		return;
+	}
+	
+	for(int i=0;i<A[x].size();i++){
+		if(!visited[A[x][i]]){
+			visited[A[x][i]] = 1;
+//			X[k] = A[x][i];
+			DFS(A[x][i],k+1,n,sum + d[x][i]);
+			visited[A[x][i]] = 0;
+		}
+	}
+}
+
+int main(){
+	//input
+	int n; cin>>n;
+	for(int i=1;i<n;i++){
+		int a,b,v;cin>>a>>b>>v;
+		A[a].push_back(b);
+		A[b].push_back(a);
+		d[a].push_back(v);
+		d[b].push_back(v);
+	}
+	//solve
+//	X[0] = 1;
+	visited[1] = 1;
+	DFS(1,1,n,0);
+	memset(visited,0,sizeof(visited));
+	DFS(u,1,n,0);
+	//show output
+	cout<<res;
+	
+	return 0;
+}
+
---
+++ exercise/Graph/MINIMUM SPANNING TREE.cpp
@@ -0,0 +1,78 @@
+/*
+inputCopy
+5 8
+1 2 1
+1 3 4
+1 5 1
+2 4 2
+2 5 1
+3 4 3
+3 5 3
+4 5 2
+outputCopy
+7
+
+*/
+
+#include<iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+struct Graph{
+	int x;
+	int y;
+	int v;
+};
+vector <Graph> g;
+int fron[100001], cnt[100001];
+bool cmp(Graph g1 , Graph g2){
+	return g1.v < g2.v;
+}
+
+int find(int u) {
+    if (fron[u] != u) fron[u] = find(fron[u]);
+    return fron[u];
+}
+
+bool join(int u, int v) {
+    u = find(u); v = find(v);
+    if (u == v) return false;
+    
+    if (cnt[u] == cnt[v]) cnt[u]++;
+    if (cnt[u] < cnt[v]) 
+		fron[u] = v;
+    else 
+		fron[v]=u;
+    return true;
+}
+
+
+
+int main(){
+	//input
+	int n,m;cin>>n>>m;
+	for(int i=0;i<m;i++){
+		Graph g1;
+		cin>>g1.x>>g1.y>>g1.v;
+		g.push_back(g1);
+	}
+	sort(g.begin(),g.end(),cmp);
+	for (int i=1; i<=n; i++) {
+        fron[i] = i;
+        cnt[i] = 0;
+    }
+	//solve
+	long long res = 0;
+	for(int i=0;i<g.size();i++){
+		if(join(g[i].x,g[i].y)){
+			res += g[i].v;
+		}
+	}
+	//show output	
+	cout<<res;
+		
+	return 0;
+}
+
